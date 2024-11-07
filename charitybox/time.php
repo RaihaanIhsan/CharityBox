e3b70c5a79da28e2a387e6c2d5e0ad94ec7b03c4
RaihaanIhsan <raihaanihsan21001@gmail.com>
2024-11-07 17:02:10 +0500

    Updated various files, added new pages, and modified existing ones.

--- charitybox/time.php
+++
@@ -1,6 +0,0 @@
-<?php
-// date_default_timezone_set('Asia/Karachi'); // Set it explicitly for testing
-echo 'Current PHP Time Zone: ' . date_default_timezone_get() . '<br>';
-echo 'Current Date and Time: ' . date('Y-m-d H:i:s');
-?>
-
